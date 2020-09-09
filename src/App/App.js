3d200d9583290118b40de03c4cc2db4498cf13fa
Allison Wagner <allisonjw01@hotmail.com>
2020-09-09 13:52:17 -0600

    Imports seachform into App

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import '../styles/index.css';
+import { SearchForm } from '../SearchForm/SearchForm';
 
 export const App = () => {
   return (
     <div className="App">
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchForm />
     </div>
   );
 }
