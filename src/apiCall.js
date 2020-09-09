dedff36541c244b83c5bb25ab651fecc87d80e3e
Allison Wagner <allisonjw01@hotmail.com>
2020-09-09 13:52:46 -0600

    Fixes the data comming back from the api call

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,12 +1,12 @@
 const key = `dc540c9b2a9f44d4b08417b1b2d2a919`;
 
 export const getSearchApi = async (query) => {
-    const url = `https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${query}&number=10`
+    const url = `https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${query}`
     const response = await fetch(url);
     if (!response.ok) {
       throw Error('Error searching for the recipe');
     }
     const data = await response.json();
-    console.log('apicall', data.results)
+    console.log('apicall', data)
     return data
 };
