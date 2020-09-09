7092af560323f58d0d36b56275f7edacd1215acb
Allison Wagner <allisonjw01@hotmail.com>
2020-09-09 13:23:03 -0600

    Adds file for fetching api and function to do that on search

---
+++ src/apiCall.js
@@ -0,0 +1,12 @@
+const key = `dc540c9b2a9f44d4b08417b1b2d2a919`;
+
+export const getSearchApi = async (query) => {
+    const url = `https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${query}&number=10`
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw Error('Error searching for the recipe');
+    }
+    const data = await response.json();
+    console.log('apicall', data.results)
+    return data
+};
