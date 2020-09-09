5fe9bff9f1705078ea41e487a5d9ec08ac858494
Allison Wagner <allisonjw01@hotmail.com>
2020-09-09 16:49:44 -0600

    Ceating the global store for the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { rootReducer } from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
